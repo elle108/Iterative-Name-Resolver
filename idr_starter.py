907b4238167be700690523243d19f27127d1cc28
Elle Louise Miller <ellemiller108@gmail.com>
2025-10-03 20:10:33 -0400

    Update idr_starter.py

--- idr_starter.py
+++ idr_starter.py
@@ -242,20 +242,3 @@ if __name__ == '__main__':
         print(f"\n(Error) Failed to resolve {domain}")
   finally:
     sock.close()
-
-
-"""
-
-Resources Used:
-
-1. https://pythontic.com/modules/socket/gethostbyname#google_vignette
-
-2. https://artofproblemsolving.com/wiki/index.php/Range_function?srsltid=AfmBOor_-WXS4sGEHylIsS-87jOOKEF5MDyIjtaIHHeU49ln3JYJhUjv
-
-3. https://realpython.com/python-join-string/#:~:text=Python's%20built%2Din%20string%20method,()%20to%20concatenate%20strings%20effectively.
-
-4. https://stackoverflow.com/questions/14625693/find-http-and-or-www-and-strip-from-domain-leaving-domain-com
-
-5. VSCode inline suggestions and Copilot for debugging and syntax help
-
-"""
